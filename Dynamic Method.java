b18054d742f223bd8a0e040bc15eb50163e9796f
ramakrishnaaaaa <ramakrishnaa399@gmail.com>
2025-11-15 22:17:47 +0530

    Create Dynamic Method.java

---
+++ Dynamic Method.java
@@ -0,0 +1,27 @@
+public class Dynamic {
+    public static void main(String[] args) {
+                
+        Animal a= new Animal(); // reference of parent class
+        a.sound();
+        a = new Dog();   // object of child class Dog
+        a.sound();       // Output: Bark
+        a = new Cat();   // object of child class Cat
+        a.sound();       // Output: Meow
+    }
+ }
+ Syntax Example
+ class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+ }
+ class Dog extends Animal {
+    void sound() {
+        System.out.println("Bark");
+    }
+ }
+ class Cat extends Animal {
+    void sound() {
+        System.out.println("Meow");
+    }
+ }
